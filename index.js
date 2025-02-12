5659dc1ccfa2698e623ca82dc79d7e78143b8560
dustinpfister <dustin.pfister@gmail.com>
2025-02-12 08:16:14 -0500

    fixed isshue with depth, but creating more chiled objects each time I enter a door

--- index.js
+++ index.js
@@ -30,13 +30,38 @@ class Load extends Phaser.Scene {
 class World extends Phaser.Scene {
 
     setupMap ( mapNum = 1 ) {
-    
         const map = this.map = this.make.tilemap({ key: 'map' + mapNum, tileWidth: 16, tileHeight: 16 }); 
-        map.setCollision([0,2]);
+        map.setCollision( [ 0, 2] );
         const tiles = map.addTilesetImage('map_16_16');
         const layer = this.layer = map.createLayer(0, tiles, 0, 0);
+        layer.depth = -1;
         this.physics.world.setBounds(0,0, map.widthInPixels, map.heightInPixels);
         this.mapData = this.cache.json.get('map' + mapNum + '_data');
+        
+        this.children.sortByDepth(this.player, this.map);
+        
+        console.log(this.children.length);
+        
+    
+    }
+    
+    doorCheck () {
+    
+       const doors = this.mapData.doors;
+       
+       let i = doors.length;
+       while(i--){
+           const d = doors[i];
+           const p = d.position;
+           const playerX = Math.floor( this.player.x / 16); 
+           const playerY = Math.floor( this.player.y / 16);
+           
+           if( playerX === p.x && playerY === p.y ){
+               console.log( 'DOOR! ' + d.to.mapNum + ',' + d.to.doorIndex );
+               this.setupMap(d.to.mapNum);
+           }
+           
+       }
     
     }
 
@@ -48,17 +73,21 @@ class World extends Phaser.Scene {
         // input cursors
         this.cursors = this.input.keyboard.createCursorKeys();
      
-        // map
-        this.setupMap(2);
+     
+        const x = 16 * 2 + 8;
+        const y = 16 * 2 + 8;
         
-        // player sprite
-        const x = 16 * this.mapData.spawnAt.x + 8;
-        const y = 16 * this.mapData.spawnAt.y + 8;
         this.player = this.physics.add.sprite(x, y, 'map_16_16');
         this.player.setCollideWorldBounds(true);
-        this.physics.add.collider(this.player, this.layer);    
+        this.physics.add.collider(this.player, this.layer);
+        this.player.depth = 2;
+     
+        // map
+        this.setupMap(2);
+            
         
         this.text_player = this.add.text(0, 0, 'X').setFontFamily('Monospace').setFontSize(12);
+        this.text_player.depth = 1;
         
         
     }
@@ -90,6 +119,8 @@ class World extends Phaser.Scene {
         this.text_player.x = this.player.body.position.x - 20;
         this.text_player.y = this.player.body.position.y - 20;
         this.text_player.text = Math.floor(this.player.x) + ', ' + Math.floor(this.player.y);
+        
+        this.doorCheck();
         
     }
 }
